3d520a7d61c32b88e210918c254f5333d4711bcc
Abraham Benites <abenitesvil@me.com>
2012-04-04 21:22:15 -0500

    Aplicacion creada

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TuCancha::Application.config.secret_token = '57dae67a9944fbf8db4b801a7165618223df079f7b2bbb08b1f53e5c549814d68bf6a6881240f28944bda620b661e76cd8b22e23ab57c1de4c1599b21a05248e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+TuCancha::Application.config.session_store :cookie_store, key: '_Tu-Cancha_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# TuCancha::Application.config.session_store :active_record_store
